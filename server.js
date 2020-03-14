5be082f454e1168a56444e0cf48d3ff80499d10c
jennybeka <jennybekadutra@gmail.com>
2020-03-14 23:08:43 +0100

    habilitando package.json para Heroku-v5

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<app-root>'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<app-root>/index.html'));
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
 // Start the app by listening on the default Heroku port
